4ef8dcaa6099baa998953b5f86c75baf42bd8d97
Aman Khare <khareaman754@gmail.com>
2025-04-16 02:54:13 +0530

    chore: adding title field in transaction model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema(
   {
+    title: {
+      type: String,
+      required: true,
+    },
+
     amount: {
       type: Number,
       required: true,
